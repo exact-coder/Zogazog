d4f31e52a6834974e2be531aa48a84b11da55387
Programming Bees <hasanakash799@gmail.com>
2024-03-29 02:10:27 +0600

    chore:working in main_admin js

--- static/js/main_admin.js
+++ static/js/main_admin.js
@@ -1,1 +1,18 @@
+/**
+ * Variables
+ */
+
+const chatRoom = document.querySelector("#room_uuid").textContent.replaceAll('"','')
+console.log(chatRoom);
+
+let chatSocket = null;
+
+/**
+ * Elements
+ */
+const chatLogElement = document.querySelector("#chat_log");
+const chatInputElement = document.querySelector("#chat_message_input");
+const chatSubmitElement = document.querySelector("#chat_message_submit");
+
+
 
