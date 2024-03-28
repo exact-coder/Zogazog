db791e1be029fe031de5d09cc41e3b239d11427e
Programming Bees <hasanakash799@gmail.com>
2024-03-28 12:45:41 +0600

    chore: message showing successfully

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,7 +2,8 @@ import json
 
 from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
-
+from chat.templatetags.chatextras import initials
+from django.utils.timesince import timesince
 
 class ChatConsumer(AsyncWebsocketConsumer):
 
@@ -17,3 +18,33 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         # Leave room
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name) # type: ignore
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        type = text_data_json['type']
+        message = text_data_json['message']
+        name = text_data_json['name']
+        agent = text_data_json.get('agent','')
+        print(type)
+
+        if type == 'message':
+            await self.channel_layer.group_send( # type: ignore
+                self.room_group_name, {
+                    'type':'chat_message',
+                    'message': message,
+                    'name': name,
+                    'agent': agent,
+                    'initials': initials(name),
+                    'created_at': '',#timesince(new_message.created_at),
+                }
+            )
+    async def chat_message(self,event):
+        # Send message to Websocket (frontend)
+        await self.send(text_data=json.dumps({
+            'type': event['type'],
+            'message': event['message'],
+            'name': event['name'],
+            'agent': event['agent'],
+            'initials': event['initials'],
+            'created_at': event['created_at'],
+        }))
--- static/js/main.js
+++ static/js/main.js
@@ -45,6 +45,49 @@ function getCookie(name) {
     return cookieValue;
 }
 
+function sendMessage(){
+    chatSocket.send(JSON.stringify({
+        'type': 'message',
+        'message': chatInputElement.value,
+        'name': chatName,
+
+    }))
+    chatInputElement.value = ''
+}
+
+function onChatMessage(data){
+    console.log('onChatMessage',data);
+
+    if (data.type == 'chat_message') {
+        if (data.agent) {
+            chatLogElement.innerHTML += `
+            <div class="flex w-full mt-2 space-x-3 max-w-md">
+                <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+                <div>
+                    <div style="background-color:rgb(209 213 219);" class="bg-gray-300 p-3 rounded-l-lg rounded-br-lg">
+                        <p class="text-sm">${data.message}</p>
+                    </div>
+                    <span class="text-xs text-gray-500 leading-none" >${data.created_at} ago</span>
+                </div>
+                
+            </div>
+            `
+        }else{
+            chatLogElement.innerHTML += `
+            <div class="flex w-full mt-2 space-x-3 max-w-md ml-auto justify-end">
+                <div>
+                    <div style="background-color:rgb(147 197 253);" class="bg-blue-300 p-3 rounded-l-lg rounded-br-lg">
+                        <p class="text-sm">${data.message}</p>
+                    </div>
+                    <span class="text-xs text-gray-500 leading-none" >${data.created_at} ago</span>
+                </div>
+                <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300 text-center pt-2">${data.initials}</div>
+            </div>
+            `
+        }
+    } 
+}
+
 async function joinChatRoom(){
     chatName = chatNameElement.value;
     
@@ -69,6 +112,7 @@ async function joinChatRoom(){
 
     chatSocket.onmessage = function(e){
         console.log('OnMessage');
+        onChatMessage(JSON.parse(e.data));
     }
     
     chatSocket.onopen = function(e){
@@ -101,4 +145,11 @@ chatJoinElement.onclick = function(e){
     joinChatRoom()
 
     return false
+}
+
+chatSubmitElement.onclick = function(e) {
+    e.preventDefault()
+    sendMessage()
+
+    return false;
 }
